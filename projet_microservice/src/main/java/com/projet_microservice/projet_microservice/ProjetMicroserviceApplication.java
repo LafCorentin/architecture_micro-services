978f5343e77d3a9ffa3aaf39d329862a7576b10f
lmommeja <lmommeja@FRLT061.vermeg.com>
2018-06-07 10:34:55 +0200

    Import du pom.xml de Spring Intitilasr

---
+++ projet_microservice/src/main/java/com/projet_microservice/projet_microservice/ProjetMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.projet_microservice.projet_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetMicroserviceApplication.class, args);
+	}
+}

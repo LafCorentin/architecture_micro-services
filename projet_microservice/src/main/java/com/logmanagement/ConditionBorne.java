f21dc6eaf8bfd8525299866031dafa7b7ecd6528
lmommeja <lmommeja@FRLT061.vermeg.com>
2018-06-11 10:07:50 +0200

    Appli spring se lance + Condition borne.java

---
+++ projet_microservice/src/main/java/com/logmanagement/ConditionBorne.java
@@ -0,0 +1,37 @@
+package com.logmanagement;
+
+import com.logs.GeneLog;
+import java.util.Date;
+
+public class ConditionBorne implements ConditionOnLog {
+	Date borne_inf;
+	Date borne_sup;
+	
+	public ConditionBorne(Date inf, Date sup) {
+		borne_inf = inf;
+		borne_sup = sup;
+	}
+	
+	public Date getInf()  {  
+		return borne_inf;
+	}
+	
+	public Date getSup() {
+		 return borne_sup;
+	}
+	
+	public void setSup(Date sup) {
+		borne_sup = sup;
+	}
+	
+	public void setInf(Date inf) {
+		borne_inf = inf;
+	}
+	
+	@Override
+	public boolean tester(GeneLog log) {
+		Date date_log = log.getDate();
+		return (date_log.after(borne_inf) && date_log.before(borne_sup));
+	}
+}
+

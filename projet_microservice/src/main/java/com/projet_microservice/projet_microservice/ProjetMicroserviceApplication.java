d34c5648cb6ab2c6cd263d9cb6eead630cbf5f48
mommejal <leonard.mommeja@gmail.com>
2018-06-11 11:03:01 +0200

    affichage d'un texte marche sur le web

--- projet_microservice/src/main/java/com/projet_microservice/projet_microservice/ProjetMicroserviceApplication.java
+++ projet_microservice/src/main/java/com/projet_microservice/projet_microservice/ProjetMicroserviceApplication.java
@@ -1,14 +1,11 @@
 package com.projet_microservice.projet_microservice;
 
-//System.setProperty("javax.net.ssl.trustStore", "C:\Users\lmommeja\Desktop\keystore.jks");
-//System.setProperty("javax.net.ssl.trustStorePassword", changeit);
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjetMicroserviceApplication {
 	public static void main(String[] args) {
-		System.out.println("Wesh");
 		SpringApplication.run(ProjetMicroserviceApplication.class, args);
 	}
 }

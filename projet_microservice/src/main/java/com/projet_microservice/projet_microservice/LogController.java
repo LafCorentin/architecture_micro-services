09bfb367ae47b625d4f65c5f86e9000737674aaa
mommejal <leonard.mommeja@gmail.com>
2018-06-12 10:12:40 +0200

    Fonctions findAll, findByID, findBySeverityLvl en cours d'implémentation

--- projet_microservice/src/main/java/com/projet_microservice/projet_microservice/LogController.java
+++ projet_microservice/src/main/java/com/projet_microservice/projet_microservice/LogController.java
@@ -1,25 +1,47 @@
-package com.projet_microservice;
+package com.projet_microservice.projet_microservice;
 import org.springframework.web.bind.annotation.*;
 import com.logs.logXMLTest;
-import org.springframework.stereotype.Controller;
 import com.logs.GeneLog;
-//import java.lang.String;
-//import java.util.Date;
+import com.dao.LogDao;
+import com.dao.GeneLogDaoImpl;
+import org.springframework.stereotype.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.String;
+import java.util.Date;
 
 @RestController
 public class LogController {
+	
+	    @Autowired(required=false)
+	    public GeneLogDaoImpl LogDao;
+	
+	    //Récupérer la liste compleste des logs
 	    @RequestMapping(value="/ListeLogs", method=RequestMethod.GET)
-	    public String ListeLogs() {
-	        return "Ceci est un exemple de log sous forme de string";
+	    public List <GeneLog> listeLogs() {
+	        return LogDao.findAll();
+	    }
+	   
+	    
+	    @GetMapping(value = "/SeverityLvl/{lvl}")
+	    public String afficherUnLogSeverityLvl(@PathVariable int lvl) {
+	    	return (LogDao.findBySeverityLvl(lvl)).toString();
 	    }
 	    
-//	    @GetMapping(value = "/ListeLogs/{id}")
-//	    public String afficherUnLog(GeneLog log) {
-//	    	return log.getData();
-//	    }
+	    @GetMapping(value = "/afficherUnLog")
+	    public String afficherUnLog() {
+	    	// Devra prendre en argument un log et l'afficher
+	    	logXMLTest log = new logXMLTest(new String("Contenu du log d'Id="));
+	        return (log.getData()+log.getID());
+	    }
+	    
+
+
 	    
 //	    @GetMapping(value = "/afficherUnLog")
 //	    public String afficherUnLog() {
+//	    	// Devra prendre un argument un log et son iD et l'afficher
 //	    	logXMLTest log = new logXMLTest(new String("Contenu du log"));
 //	        return log.getData();
 //	    }

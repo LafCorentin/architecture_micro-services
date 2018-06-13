a77e0f16aad6c79f448587455057da26146e5f30
clafond <corentin.lafond@gmail.com>
2018-06-13 15:13:31 +0200

    le dernier trait aavant poubelle

--- projet_microservice/src/main/java/com/logevent/LogEvent.java
+++ projet_microservice/src/main/java/com/logevent/LogEvent.java
@@ -1,6 +1,6 @@
 package com.logevent;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.*;
 
 public class LogEvent extends ApplicationEvent {
 	private String message;
